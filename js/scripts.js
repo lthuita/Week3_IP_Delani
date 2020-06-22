da372be5bc01469f73e93aec4f4ba5477bcd5194
lthuita <lthuita@gmail.com>
2020-06-22 23:00:37 +0300

    added the jquery effect on the portfolio images. By hovering the mouse over the image, the text is shown and the image fades

---
+++ js/scripts.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+  $("#portfolio1").mouseover(function(){
+    $("#work1").show();
+    $("#img1").fadeTo(1, 0.15);
+  }).mouseout(function(){
+    $("#work1").hide();
+    $("#img1").fadeTo(1, 1.0);
+  });
+});
+$(document).ready(function(){
+  $("#portfolio2").mouseover(function(){
+    $("#work2").show();
+    $("#img2").fadeTo(1, 0.15);
+  }).mouseout(function(){
+    $("#work2").hide();
+    $("#img2").fadeTo(1, 1.0);
+  });
+});
+$(document).ready(function(){
+  $("#portfolio3").mouseover(function(){
+    $("#work3").show();
+    $("#img3").fadeTo(1, 0.15);
+  }).mouseout(function(){
+    $("#work3").hide();
+    $("#img3").fadeTo(1, 1.0);
+  });
+});
+$(document).ready(function(){
+  $("#portfolio4").mouseover(function(){
+    $("#work4").show();
+    $("#img4").fadeTo(1, 0.15);
+  }).mouseout(function(){
+    $("#work4").hide();
+    $("#img4").fadeTo(1, 1.0);
+  });
+});
+$(document).ready(function(){
+  $("#portfolio5").mouseover(function(){
+    $("#work5").show();
+    $("#img5").fadeTo(1, 0.15);
+  }).mouseout(function(){
+    $("#work5").hide();
+    $("#img5").fadeTo(1, 1.0);
+  });
+});
+$(document).ready(function(){
+  $("#portfolio6").mouseover(function(){
+    $("#work6").show();
+    $("#img6").fadeTo(1, 0.15);
+  }).mouseout(function(){
+    $("#work6").hide();
+    $("#img6").fadeTo(1, 1.0);
+  });
+});
+$(document).ready(function(){
+  $("#portfolio7").mouseover(function(){
+    $("#work7").show();
+    $("#img7").fadeTo(1, 0.15);
+  }).mouseout(function(){
+    $("#work7").hide();
+    $("#img7").fadeTo(1, 1.0);
+  });
+});
+$(document).ready(function(){
+  $("#portfolio8").mouseover(function(){
+    $("#work8").show();
+    $("#img8").fadeTo(1, 0.15);
+  }).mouseout(function(){
+    $("#work8").hide();
+    $("#img8").fadeTo(1, 1.0);
+  });
+});
