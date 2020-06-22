deb167020311868b667877e76757d4acad9a027c
lthuita <lthuita@gmail.com>
2020-06-23 00:31:01 +0300

    troubleshooting on the contact form connection to the mail-chimp

--- js/scripts.js
+++ js/scripts.js
@@ -101,3 +101,15 @@ $(document).ready(function(){
       $("#product-image").show();
     });
 });
+$(document).ready(function(){
+  $("form#mc-embedded-subscribe-form").submit(function(event){
+    var name = $("input#MERGE1").val();
+    var email = $("input#MERGE0").val();
+    var message = $("textarea#comment").val();
+    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+      alert (name + ", we have received your message. Thank you for reaching out to us.");
+    }
+    else {
+      alert("Please enter your name and email!");
+    }    
+  });
